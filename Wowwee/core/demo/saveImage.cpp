39f99cafd36b08c55312ee0ecf8832dc8c6fe817
Zhangziyang14 <1203294279@qq.com>
2015-08-18 10:40:21 +0800

    Add some demo and thirdpart lib
    add findpinksquare, object tracking using TLD, also change the saveImage
    function。

---
+++ Wowwee/core/demo/saveImage.cpp
@@ -0,0 +1,48 @@
+#include "robotdriver.h"
+#include "robotcolor.h"
+#include <iostream>
+#include <string>
+
+int main() {
+    int major, minor;
+    IplImage *image = NULL;
+   const char *IP = "192.168.10.18";
+    // Setup the robot interface
+    RobotInterface *robot = new RobotInterface(IP, 0);
+
+    // Print the API Version
+    robot->APIVersion(&major, &minor);
+    std::cout << "Robot API Test: API Version v" << major << "." << minor << std::endl;
+
+    // Setup the camera
+    if(robot->cameraConfigure(RI_CAMERA_DEFAULT_BRIGHTNESS, RI_CAMERA_DEFAULT_CONTRAST, 30, RI_CAMERA_RES_640, RI_CAMERA_QUALITY_HIGH)) {
+        std::cout << "Failed to configure the camera!" << std::endl;
+        exit(-1);
+    }
+
+    // Create a window to display the output
+    cvNamedWindow("Rovio Camera", CV_WINDOW_AUTOSIZE);
+
+
+    // Create an image to store the image from the camera
+    image = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 3);
+
+    // Move the head up to the middle position
+     robot->move(RI_HEAD_MIDDLE, RI_FASTEST);
+    // Update the robot's sensor information
+ for(int image_number =0 ;image_number >= 0; image_number ++){
+     robot->getImage(image);
+
+     cvShowImage("Rovio Camera", image);
+     cvWaitKey(22);
+     robot->saveImage(image,image_number);
+
+    printf("%d",image_number);
+};
+
+
+    // Clean up (although we'll never get here...)
+    delete(robot);
+
+    return 0;
+}
